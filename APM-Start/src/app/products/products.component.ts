49faf27ce99f8c8ba999a64e156bbc6bd7f4212e
awais <awais.malik.q@gmail.com>
2019-05-28 15:53:38 +0500

    my new project

---
+++ APM-Start/src/app/products/products.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+ pageTitle:string ="Product List";
+ imgWidth:number =50;
+ imgHeight:number=50;
+products: any[] = [
+  {
+  productId:1,
+  productName:"Grand Cart",
+  productCode:"GDN-00677",
+  releaseDate:"18 Mar 2019",
+  descripton:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+  price:5464,
+  starRating:4.3,
+  imageUrl:"https://estarelektro.nl/image/cache/data/RENNSTEIG/102_6948--0101_N-525x525.gif"
+},
+{
+  productId:2,
+  productName:"Hammar",
+  productCode:"GDN-9489",
+  releaseDate:"18 Mar 2017",
+  descripton:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+  price:5655,
+  starRating:3.3,
+  imageUrl:"https://images-na.ssl-images-amazon.com/images/I/81u7Z4TdueL._SL1500_.jpg"
+}
+]
+
+
+
+
+
+
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
